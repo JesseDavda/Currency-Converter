c032b2a9d2567a1925caa3c7369792a8228debed
JesseDavda <jessepilot@live.com>
2018-07-07 16:48:30 +0200

    redesign

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,14 @@
-while(1 == 1) {
-    var from = document.getElementById('select_1').value;
-    var to = document.getElementById('select_2').value;
-    var value = document.getElementById('input_1').value;
+document.getElementById('go').onclick = () => {
+    console.log('bye');
 
-    axios.get(`http://data.fixer.io/api/convert?access_key=&from=${from}&to=${to}&amount=${value}`, (data) => {
+    var from = document.getElementById('select_1').value;
+    console.log(from);
+    var to = document.getElementById('select_2').value;
+    console.log(to);
+    var value = document.getElementById('input_1').value;
+    console.log(value);
+
+    axios.get(`https://www.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=ultra`).then(data => {
         console.log(data);
     });
-
 }
